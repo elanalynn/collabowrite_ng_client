869b91d7949b5394004e12828b7a88ce29e57553
Elana Kopelevich <ekopelevich@gmail.com>
2016-10-12 11:42:38 -0600

    show/hide main nav items based on user login

--- public/javascripts/controllers/MainController.js
+++ public/javascripts/controllers/MainController.js
@@ -1,8 +1,11 @@
 function MainController(userService) {
   const vm = this
 
-  userService.getLoggedInUser().then(user => {
+  userService.getLoggedInUser()
+  .then(user => {
+    console.log('user', user.data.user)
     if (!user) vm.user = false
-    else vm.user = user
+    else vm.user = user.data.user.displayName
+    console.log('vm.user', vm.user)
   })
 }
