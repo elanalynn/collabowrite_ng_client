10d4f7982d68208f93a1261749e7d8cc47c30aae
Elana Kopelevich <ekopelevich@gmail.com>
2016-10-12 10:48:26 -0600

    update auth link

--- public/javascripts/controllers/MainController.js
+++ public/javascripts/controllers/MainController.js
@@ -1,4 +1,8 @@
-function MainController() {
+function MainController(userService) {
   const vm = this
 
+  userService.getLoggedInUser().then(user => {
+    if (!user) vm.user = false
+    else vm.user = user
+  })
 }
--- public/javascripts/services/userService.js
+++ public/javascripts/services/userService.js
@@ -1,12 +1,9 @@
 function userService($http, $stateParams){
   let service = {}
 
-  service.getUsers = function() {
-    return $http.get('http://localhost:3000/api/v1/users')
-  }
-  service.getUser = function() {
-    return $http.get(`http://localhost:3000/api/v1/users/${$stateParams.id}`)
-  }
+  service.getLoggedInUser = () => $http.get('http://localhost:3000/api/v1/auth')
+  service.getUsers = () => $http.get('http://localhost:3000/api/v1/users')
+  service.getUser = () => $http.get(`http://localhost:3000/api/v1/users/${$stateParams.id}`)
 
   return service
 }
