c538e7e90f236244cedaf1f6a7e8631c86b87cdf
Elana Kopelevich <ekopelevich@gmail.com>
2016-10-12 12:50:48 -0600

    login/logout

--- public/javascripts/controllers/MainController.js
+++ public/javascripts/controllers/MainController.js
@@ -3,9 +3,8 @@ function MainController(userService) {
 
   userService.getLoggedInUser()
   .then(user => {
-    console.log('user', user.data.user)
     if (!user) vm.user = false
-    else vm.user = user.data.user.displayName
+    else vm.user = user.data.user
     console.log('vm.user', vm.user)
   })
 }
