3d5d16429d51b86b0d950ce777804424e5c94eda
Elana Kopelevich <ekopelevich@gmail.com>
2016-10-14 19:56:23 -0600

    save story without error - add condition in story controller to only get story in view with id param

--- public/javascripts/controllers/StoryController.js
+++ public/javascripts/controllers/StoryController.js
@@ -1,20 +1,18 @@
 function StoryController($stateParams, $location, userService,  storyService) {
   const vm = this
 
-  vm.id = $stateParams.storyId
-  storyService.getStory(vm.id).then(story => vm.story = story.data)
+  if ($stateParams.id) {
+    storyService.getStory($stateParams.id).then(story => vm.story = story.data)
+  }
+
   storyService.getStories().then(stories => vm.stories = stories.data.data)
   userService.getLoggedInUser().then(user => vm.user = user)
 
-  vm.story = {}
-
   vm.createStory = story => {
+    const newStory = story
     userService.getLoggedInUser()
-    .then(user => vm.story.user_id = user.data.user.id)
-    .then(() => storyService.createStory(story)
-      .then(response => {
-        const id = response.data.id
-        $location.url(`/stories/${id}`)
-      }))
+    .then(user => newStory.user_id = user.data.user.id)
+    .then(() => storyService.createStory(newStory))
+    .then(response => $location.url(`/stories/${response.data.id}`))
   }
 }
