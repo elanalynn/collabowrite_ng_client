917582f4cde5eb86f5e6807d1757ad70cfe88047
Elana Kopelevich <ekopelevich@gmail.com>
2016-10-16 21:51:33 -0600

    getting chapter and user data for story, but not diplaying in view

--- public/javascripts/controllers/StoryController.js
+++ public/javascripts/controllers/StoryController.js
@@ -1,15 +1,26 @@
 function StoryController($stateParams, $location, userService,  storyService) {
   const vm = this
+  vm.story = {}
 
   if ($stateParams.id) {
-    storyService.getStory($stateParams.id).then(story => vm.story = story.data)
+    storyService.getStory($stateParams.id)
+    .then(story => {
+      vm.story = story.data
+      return vm.story
+    })
+    .then(story => Promise.all([userService.getUser(story.user_id), storyService.getChapters(story.id)])
+      .then(data => {
+        vm.story.author = `${data[0].data.first_name} ${data[0].data.last_name}`
+        vm.story.chapters = data[1].data.data
+        console.log(vm.story.author, vm.story.chapters)
+      }))
+    console.log(vm.story.author, vm.story.chapters)
   }
 
   storyService.getStories().then(stories => {
     vm.stories = stories.data.data
-    console.log(vm.stories)
   })
-  
+
   userService.getLoggedInUser().then(user => vm.user = user)
 
   vm.createStory = story => {
--- public/javascripts/services/storyService.js
+++ public/javascripts/services/storyService.js
@@ -1,9 +1,10 @@
-function storyService($http, $stateParams){
-  
+function storyService($http){
+
   let service = {}
 
   service.getStories = () => $http.get('http://localhost:3000/api/v1/stories')
-  service.getStory = () => $http.get(`http://localhost:3000/api/v1/stories/${$stateParams.id}`)
+  service.getStory = (id) => $http.get(`http://localhost:3000/api/v1/stories/${id}`)
+  service.getChapters = (id) => $http.get(`http://localhost:3000/api/v1/stories/${id}/chapters`)
   service.createStory = story => $http.post('http://localhost:3000/api/v1/stories', story)
 
   return service
