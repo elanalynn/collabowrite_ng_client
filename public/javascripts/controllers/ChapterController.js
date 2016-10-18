6e0a069ef4f0a5034d20ecc402ffe38399d39753
Elana Kopelevich <ekopelevich@gmail.com>
2016-10-18 14:02:52 -0600

    display new chapter info on single chapter view after creating new chapter

--- public/javascripts/controllers/ChapterController.js
+++ public/javascripts/controllers/ChapterController.js
@@ -1,24 +1,30 @@
 function ChapterController($stateParams, $location, userService, storyService, chapterService) {
   const vm = this
 
-  storyService.getStory($stateParams.storyId)
-  .then(story => vm.story = story.data)
+  console.log('$stateParams', $stateParams)
 
-  chapterService.getChapters($stateParams.storyId).then(chapters => {
-    vm.chapters = chapters.data.data
-    vm.last_chapter = vm.chapters[vm.chapters.length - 1]
-  })
+  if ($stateParams.storyId) {
+    storyService.getStory($stateParams.storyId)
+    .then(story => vm.story = story.data)
+
+    chapterService.getChapters($stateParams.storyId).then(chapters => {
+      vm.chapters = chapters.data.data
+      vm.last_chapter = vm.chapters[vm.chapters.length - 1]
+    })
+  }
 
   if ($stateParams.chapterId) {
-    chapterService.getChapter($stateParams.chapterId)
+    chapterService.getChapter($stateParams.storyId, $stateParams.chapterId)
     .then(chapter => {
       vm.chapter = chapter.data
-      return vm.chapter
+      userService.getUser(chapter.data.user_id)
+      .then(details => {
+        vm.chapter.user = `${details.data.first_name} ${details.data.last_name}`
+      })
     })
   }
 
   vm.createChapter = chapter => {
-    console.log(chapter)
     const newChapter = chapter
     userService.getLoggedInUser()
     .then(user => {
@@ -26,7 +32,6 @@ function ChapterController($stateParams, $location, userService, storyService, c
       newChapter.user_id = user.data.user.id
       return chapterService.createChapter(newChapter)
       .then(res => {
-        console.log('responsarino', res)
         $location.url(`stories/${newChapter.story_id}/chapters/${res.data.data[0]}`)
       })
     })
--- public/javascripts/services/chapterService.js
+++ public/javascripts/services/chapterService.js
@@ -3,7 +3,7 @@ function chapterService($http){
   let service = {}
 
   service.getChapters = id => $http.get(`http://localhost:3000/api/v1/stories/${id}/chapters`)
-  service.getChapter = chapter => $http.get(`http://localhost:3000/api/v1/stories/${chapter.story_id}/chapters/${chapter.id}`)
+  service.getChapter = (storyId, chapterId) => $http.get(`http://localhost:3000/api/v1/stories/${storyId}/chapters/${chapterId}`)
   service.createChapter = chapter => $http.post(`http://localhost:3000/api/v1/stories/${chapter.story_id}/chapters`, chapter)
 
 
