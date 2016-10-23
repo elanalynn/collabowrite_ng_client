189b87b623ce7d9648da0e2645a0b6645d52a9ca
Elana Kopelevich <ekopelevich@gmail.com>
2016-10-23 15:26:54 -0600

    rmove another firebase file and update server file (listening message)

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8000
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(port, () => {
-  console.log('Listening on port 8000.')
+  console.log('Listening on port ' + port)
 })
 
 module.exports = app
