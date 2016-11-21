1791c6494c5c577bfb0a8155910e376f991199db
Elana Kopelevich <ekopelevich@gmail.com>
2016-11-21 12:14:14 -0700

    fix labels overlapping prefilled content by adding active class to labels in edit profile

--- public/javascripts/controllers/DashboardController.js
+++ public/javascripts/controllers/DashboardController.js
@@ -20,48 +20,50 @@ function DashboardController($state, $stateParams, $location, authService, userS
     }
   })
 
-  authService.getCurrentUser().then(data => {
-    if (!data.data.user) return null
-    else return data.data.user.id
-  })
-  .then(userId => {
-    Promise.all([
-      // get user
-      userService.getUser(userId).then(user => {
-        vm.user = user.data
-      }),
-      // get user stories
-      storyService.getUserStories(userId)
-      .then(stories => {
-        vm.stories = stories.data.data
-        return vm.stories.map(story => story.storyId)
-      })
-      // get user story chapters
-      .then(storyIds => {
-        storyIds.map(id => {
-        chapterService.getChapters(id).then(chapters => {
-          chapters.data.data.forEach(chapter => {
-            vm.chapters.push(chapter)
+  if (authService.isAuthorized($stateParams.id)) {
+    authService.getCurrentUser().then(data => {
+      if (!data.data.user) return null
+      else return data.data.user.id
+    })
+    .then(userId => {
+      Promise.all([
+        // get user
+        userService.getUser(userId).then(user => {
+          vm.user = user.data
+        }),
+        // get user stories
+        storyService.getUserStories(userId)
+        .then(stories => {
+          vm.stories = stories.data.data
+          return vm.stories.map(story => story.storyId)
+        })
+        // get user story chapters
+        .then(storyIds => {
+          storyIds.map(id => {
+          chapterService.getChapters(id).then(chapters => {
+            chapters.data.data.forEach(chapter => {
+              vm.chapters.push(chapter)
+              })
             })
           })
-        })
-      }),
-      // get user favorites
-      favoriteService.getFavoritesByUser(userId)
-      .then(record => record.data.map(record => record.story_id))
-      .then(storyIds => {
-        storyIds.forEach(id => storyService.getStory(id)
-        .then(story => vm.favorites.push(story.data)))
-      }),
-      // get pending
-      // pendingService.getPendingChapters(userId, 'others').then(pending => {
-      //   vm.othersPending = pending
-      //   console.log('others', vm.othersPending)
-      // }),
-      // pendingService.getPendingChapters(userId, 'mine').then(pending => {
-      //   vm.myPending = pending
-      //   console.log('my', vm.myPending)
-      // }),
-    ])
-  })
+        }),
+        // get user favorites
+        favoriteService.getFavoritesByUser(userId)
+        .then(record => record.data.map(record => record.story_id))
+        .then(storyIds => {
+          storyIds.forEach(id => storyService.getStory(id)
+          .then(story => vm.favorites.push(story.data)))
+        }),
+        // get pending
+        // pendingService.getPendingChapters(userId, 'others').then(pending => {
+        //   vm.othersPending = pending
+        //   console.log('others', vm.othersPending)
+        // }),
+        // pendingService.getPendingChapters(userId, 'mine').then(pending => {
+        //   vm.myPending = pending
+        //   console.log('my', vm.myPending)
+        // }),
+      ])
+    })
+  }  
 }
