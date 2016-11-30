d83d3ea4cbd66b11e0917cfbd74e7c342b318ccd
Elana Kopelevich <ekopelevich@gmail.com>
2016-11-30 13:27:47 -0700

    fix apiUrl

--- public/javascripts/env.js
+++ public/javascripts/env.js
@@ -1,6 +1,6 @@
 (function (window) {
   window.__env = window.__env || {}
-  window.__env.apiUrl = 'https://cw-api.elanalynn.com/api/v1'
+  window.__env.apiUrl = 'https://cw-api.herokuapp.com/api/v1'
   window.__env.baseUrl = '/'
   window.__env.enableDebug = true
 }(this))
