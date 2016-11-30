ad28842269e32d0c33f05aca1f50a6abe0cb99e8
Elana Kopelevich <ekopelevich@gmail.com>
2016-11-30 15:45:17 -0700

    add slash (typo)

--- public/javascripts/services/chapterService.js
+++ public/javascripts/services/chapterService.js
@@ -4,7 +4,7 @@ function chapterService(__env, $http){
 
   let service = {}
 
-  service.getChaptersByStory = id => $http.get(`${__env.apiUrl}stories/${id}/chapters`)
+  service.getChaptersByStory = id => $http.get(`${__env.apiUrl}/stories/${id}/chapters`)
   service.getChapter = id => $http.get(`${__env.apiUrl}/chapters/${id}`)
   service.createChapter = chapter => $http.post(`${__env.apiUrl}/chapters`, chapter)
 
