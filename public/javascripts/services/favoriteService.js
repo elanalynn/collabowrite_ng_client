1063c842ff3f8e1b79893f83f0e0576a229ada5d
Elana Kopelevich <ekopelevich@gmail.com>
2016-11-30 15:47:06 -0700

    add slash - fix typo

--- public/javascripts/services/favoriteService.js
+++ public/javascripts/services/favoriteService.js
@@ -4,7 +4,7 @@ function favoriteService(__env, $http){
 
   let service = {}
 
-  service.getFavoritesByUser = id => $http.get(`${__env.apiUrl}users/${id}/favorites`)
+  service.getFavoritesByUser = id => $http.get(`${__env.apiUrl}/users/${id}/favorites`)
   service.getFavoritesByStory = id => $http.get(`${__env.apiUrl}/stories/${id}/favorites`)
   service.setFavorite = data => $http.post(`${__env.apiUrl}/favorites`, data)
 
